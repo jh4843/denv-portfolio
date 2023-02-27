ae07abc663b8b3a6730c7c9c1217f1c99518f2e4
jh4843 <creamboy1@naver.com>
2023-02-27 21:00:10 +0900

    nextJsFirst Update
    nextJsFirst Update

---
+++ nextjs-portfolio/pages/about-me.tsx
@@ -0,0 +1,7 @@
+export default function AboutMe() {
+  return (
+    <div>
+        <h1>Introduce myself</h1>
+    </div>
+  )
+}
---
+++ nextjs-portfolio/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Denv's Portfolio</title>
+        <meta name="description" content="Coding is exciting." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Denv's Portfolio</h1>
+      
+    </>
+  )
+}
